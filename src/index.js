dff2de96d1075c566679c01ba1c7a98d4e71f507
BunniBooXx <140341233+BunniBooXx@users.noreply.github.com>
2024-01-14 01:38:57 -0500

    third commit

--- src/index.js
+++ src/index.js
@@ -2,22 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from './Navbar';
-import Login from './Login';
-import SignUp from './SignUp';
-import UserProfile from './UserProfile'; // Import UserProfile component
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';      
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/userprofile" element={<UserProfile />} />
       </Routes>
     </Router>
   </React.StrictMode>
